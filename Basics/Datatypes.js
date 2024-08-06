162df82a8c9bfa3ed714568d5f0628ab5cfbdc71
Tanmoy-Santra <tanmoysantra911@gmail.com>
2024-08-06 09:33:07 +0530

    let var const

--- Basics/Datatypes.js
+++ Basics/Datatypes.js
@@ -1,26 +1,62 @@
-{let i=0
-console.log("within another scope",i);}
-//let i=1 -- we cannot change the let variable withi in a scope
-{//{} -> is called scope we can use the same named variable .
+//========================LET==========================
+let i=3
+console.log(' i ',i);
+{
+    // console.log(' i cannot global',i);
+    
+    let i=0 
+    console.log("within another scope",i);
+    i=99
+    console.log(`re-assign ${i}`);
+    
+}
+//let i=1 -- we cannot re-decleare the let variable within a scope 
+//          -- but we can re-assign the value of let variable   
+
+// {} -> is called scope we can use the same named variable .
+
+{
     let i=9
     console.log("inside the another scope");
     console.log(i);
 }
 
-// the old js versionpeople use var likewis let , the problem is there is same named var are change aany where so now we use let instade of var
+
+//===================================VAR=======================================================
+
+
+// the old js version people use var likewis let , 
+//the problem is there is same named var are change any where so now we use let instade of var
+//for isuues in block scope and funtional scope .
+
+//it is easily re declare and re assign with in a same scope 
+//if it declare outside the function or block it act as global variable
+//var i function scoped or block scoped
 
 var j=78
 var j=5 //j value changes in to the curent value
 console.log(j)
+{
+    console.log('j is global',j);
+    
+}
+
+
+
+
+
+//===========================CONST===================================
 
 const e=90
 console.log( "typ of e --",typeof(e));
 console.log("type of i--",typeof(i));//it returns undefind because the i assign in the another scope and we print is outside the scope
 console.log("type of j--",typeof(j));
+
 // {
-//     var e;
+//      e=9;
 // }
 //const e=0 we cannot change  the value of const within the same scope or outside the scope the value remain unchanged.
+//blocked scope not reassign or redeclared any where
 
 u="udfiuuiuiui"
 l=900
